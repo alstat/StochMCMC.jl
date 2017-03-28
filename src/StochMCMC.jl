93f9620305025de64ad2581cf658450b26745b14
Al-Ahmadgaid Asaad <alstated@gmail.com>
2017-03-28 21:27:33 +0800

    Updated Includes

--- src/StochMCMC.jl
+++ src/StochMCMC.jl
@@ -2,12 +2,10 @@ module StochMCMC
 
 using Distributions
 
-include("MH.jl");
-include("HMC.jl");
-include("SGHMC.jl");
+include("MH.jl")
+include("HMC.jl")
+include("SGHMC.jl")
 
 export mcmc, MH, HMC, SGHMC
 
 end
-
-Pkg.clone(joinpath(homedir(), "Dropbox/MS THESIS/JULIA/StochMCMC.jl"))
